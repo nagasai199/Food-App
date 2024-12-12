40bd18bbbc77bd36bd136fcb022e5c896191266f
Nagasai <lknagasai@gmail>
2024-12-12 12:15:48 +0530

    small

---
+++ index.py
@@ -0,0 +1,7 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Welcome to the Python world!")
+ # snall change
+ print("food order online")
